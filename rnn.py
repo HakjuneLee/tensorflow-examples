152ee0631d420cbe05a77bd77eaeddb4c12e1ed3
Hakjune Lee <hakzoon2@gmail.com>
2018-08-03 03:34:58 +0000

    Added more examples

---
+++ rnn.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import tensorflow as tf
+import numpy as np
+
+unique = 'helo'
+y_data = [1,2,2,3]
+x_data = [np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,1,0]],dtype='f')
+
+cells = tf.nn.rnn_cell.BasicRNNCell(4)
+state = tf.zeros([1,cells.state_size])
+x_data = tf.split(0,4,x_data)
+
+outputs, state = tf.nn.rnn(cells,x_data,staet)
+
+logits = tf.reshape(tf.concat(1,otuputs),[-1,4])
+cost = tf.reduce_sum(loss)
+train_op = tf.train.RMSPropOptimizer(0.01,0.9).minimize(cost)
+
+with tf.Session() as sess:
+	tf.initialize_all_variables().run()
+	for i in range(100):
+		sess.run(train_op)
+		r0,r1,r2,r3 = sess.run(tf.argmax(logits,1))
+		print(r0,r1,r2,r3,':'unique[r0],unique[r1],unique[r2],unique[r3])
+
